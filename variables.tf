6543a34cc7afc0c55db42692eb1ad66474a11b02
sravanb441 <101820902+sravanb441@users.noreply.github.com>
2022-03-17 22:30:24 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+# azure service principal info
+variable azure {
+  default = {
+    subscription_id   = "9c249f9f-804a-4390-8b81-4b2c3dc2a75e"
+    tenant_id         = "d03c900d-9a0e-4be6-82ab-c674f80ef1c8"
+    client_id         = "42d37d77-60a7-431e-b5d7-6e5581ae6b71"
+    client_secret     = "xrc7Q~sP1HMRFuJ3jmzY7E5Ui_ym05v4eJznU"
+  }
+}
